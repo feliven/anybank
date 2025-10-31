0f91df90401452a28a8b36976e74a4c53da752a7
FVS <feliven@gmail.com>
2025-10-31 09:22:27 -0300

    two-way data binding implementado no formulário

--- src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.component.html',
-  styleUrl: './form-nova-transacao.component.css'
+  styleUrl: './form-nova-transacao.component.css',
 })
 export class FormNovaTransacaoComponent {
+  valorTransacao: number = 0;
 
+  onSubmit() {
+    console.log(this.valorTransacao);
+    console.log('enviado');
+  }
 }
